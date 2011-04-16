d44ca5d7d5d992ed9ceadc65b8bc204e3ecec4b7
Shorny <sima@compwork.cz>
2011-04-16 16:14:04 +0200

    Update files

---
+++ virtualmaster-firstboot_0.1-2.dsc
@@ -0,0 +1,14 @@
+Format: 1.0
+Source: virtualmaster-firstboot
+Binary: virtualmaster-firstboot
+Architecture: all
+Version: 0.1-2
+Maintainer: Josef Liska <jl@chl.cz>
+Standards-Version: 3.7.2
+Build-Depends: debhelper (>= 5)
+Checksums-Sha1: 
+ 1dfe1c078547a368719e576574f0c60613cd84e8 14095 virtualmaster-firstboot_0.1-2.tar.gz
+Checksums-Sha256: 
+ b410fa4d43ff6e37e5752e5ed3baad07649e36f25d61fd0485bbd82b57dd79d8 14095 virtualmaster-firstboot_0.1-2.tar.gz
+Files: 
+ 85838bb3c50c122d61f0274089c811b3 14095 virtualmaster-firstboot_0.1-2.tar.gz
---
+++ virtualmaster-scripts/virtualmaster-clean_before_template.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# Script for clean before create template on virtualmaster 
+
+# clear all logs
+echo "Clear logs start"
+`find /var/log -type f`; do > $logs; done
+echo "Clear logs done"
+
+# delete ssh keys
+echo "Delete ssh host key start"
+rm /etc/ssh/ssh_host_*
+echo "Delete ssh host key done"
+
+# delete cache
+echo "Delete cache start"
+apt-get clean
+echo "Delete cache done"
+
+# clear all settings
+echo "Clear settings start"
+echo "" > /etc/network/interfaces
+echo "" > /etc/resolv.conf
+echo "virtualmaster" > /etc/hostname
+echo "" > /etc/hosts
+
+rm /etc/virtualmaster.cfg 
+rm /etc/virtualmaster.cfg.disabled
+
+echo "Clear settings done"
+
+
+# clear root dir
+echo "Clear root start"
+rm /root/*
+echo "Clear root done"
